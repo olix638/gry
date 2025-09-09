0677d77ad5b87e425c8b5103e3dd41d09595915b
oliwier <oliwier.radziwonski@infotech.edu.pl>
2025-09-09 12:48:58 +0200

    Zaktualizowano dialog Elenor w funkcji walka1, dodając emocję nienawiści.

--- gra_z_artefaktami.py
+++ gra_z_artefaktami.py
@@ -277,7 +277,7 @@ def walka1():
                 if wybor == "1":
                     print("Elenor: jej dlacze znowu mnie uderzyłeś?")
                     input("Tomek(myśli): dlaczego to zrobiłem? i ta tak odrazu?")
-                    input("Elenor: dobra już uciekam. pa(mówi to z żalem).")
+                    input("Elenor: dobra już uciekam. pa(mówi to z żalem i nienawiścią).")
                     pos3.relacje["Tomek"]["atak"] += 5
                     break
                 elif wybor == "2":
