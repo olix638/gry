6edcbc358467c785ae3f5992c9863d7d2def1e1e
oliwier <oliwier.radziwonski@infotech.edu.pl>
2025-09-09 13:32:10 +0200

    Poprawa błędu w obliczaniu oszczędzenia w klasie Postać

--- gra_z_artefaktami.py
+++ gra_z_artefaktami.py
@@ -122,7 +122,7 @@ class Postać:
         elif protokuł == 5:
             self.głód = max(0, min(self.głód, self.mgłód))
             self.napojenie = max(0, min(self.napojenie, self.mnapojenie))
-            self.oszczędzenie1 = max(0,min(self.oszczędzenie1,100))
+            self.oszczędzenie1 = max(0,min(self.oszczędzenie,100))
         elif self.istota == "goblin":
             if protokuł == 1:
                 self.obrona = self.atak
