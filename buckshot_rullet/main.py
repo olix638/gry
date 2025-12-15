89f36f226af9ea4c3e4c5a49b8d8f345dad17549
oliwier <oliwier.radziwonski@infotech.edu.pl>
2025-12-15 16:08:20 +0100

    muihkmui

--- buckshot_rullet/main.py
+++ buckshot_rullet/main.py
@@ -36,6 +36,7 @@ class Postać:
             naboje.pop(0)
         else:
             print(f"{self.nazwa} nie ma piwa, więc nie może przeładować strzelby.")
+            return None
     def papierosy(self):
         if "papierosy" in self.pzedmioty:
             print(f"{self.nazwa} zapalił papierosa, więc odzyskał 1 zdrowie.")
@@ -45,11 +46,13 @@ class Postać:
                 self.zdrowie = self.mzdrowie
         else:
             print(f"{self.nazwa} nie ma papierosów, więc nie może zapalić.")
+            return None
     def lupa(self):
         if "lupa" in self.pzedmioty:
             print(f"lupa {self.nazwa} pozwala mu zobaczyć, że nabój to {naboje[0]}.")
         else:
             print(f"{self.nazwa} nie ma lupy, więc nie może sprawdzić naboju.")
+            return None
     def adrenalina(self, cel, rzecz):
         if "andrenalina" in self.pzedmioty:
             cel.przedmioty.remove(rzecz)
@@ -58,6 +61,7 @@ class Postać:
             self.pzedmioty.remove("andrenalina")
         else:
             print(f"{self.nazwa} nie ma adrenaliny, więc nie może zabrać przedmiotu.")
+            return None
     def inwenter(self):
         if "inwenter" in self.pzedmioty:
             if naboje[0] == "ostry":
@@ -66,6 +70,7 @@ class Postać:
                 naboje[0] = "ostry"
         else:
             print(f"{self.nazwa} nie ma inwentera, więc nie może zmienić naboju.")
+            return None
     def telefon(self):
         if "telefon jednorazowy" in self.pzedmioty:
             lo = randint(1,len(naboje)-1)
@@ -74,6 +79,9 @@ class Postać:
             self.co.append(lo)
             print(f"Tajemniczy głos mówi do {self.nazwa}: {lo+1+o} nabój to {naboje[lo]} nabój.")
             self.pzedmioty.remove("telefon jednorazowy")
+        else:
+            print(f"{self.nazwa} nie ma telefonu, więc tajemniczy głos nie może powiedzieć")
+            return None
     def tabletki(self):
         if "przeterminowane tabletki" in self.pzedmioty:
             szansa = randint(1,100)
@@ -86,6 +94,7 @@ class Postać:
             self.pzedmioty.remove("przeterminowane tabletki")
         else:
             print(f"{self.nazwa} nie ma przeterminowanych tabletek, więc nie może ich wziąć.")
+            return
     def kajdanki(self, cel):
         if "kajdanki" in self.pzedmioty:
             print(f"{self.nazwa} użył kajdanek, aby unieruchomić {cel.nazwa} na 2 tury.")
@@ -93,11 +102,13 @@ class Postać:
             cel.niezakajdankowany = False
         else:
             print(f"{self.nazwa} nie ma kajdanek, więc nie może unieruchomić przeciwnika.")
+            return None
     def pokaz_przedmioty(self):
         if self.pzedmioty:
             print(f"{self.nazwa} ma następujące przedmioty: {', '.join(self.pzedmioty)}")
         else:
             print(f"{self.nazwa} nie ma żadnych przedmiotów.")
+            return None
     def czy_zyje(self):
         return self.zdrowie > 0
     def pokaz_stan(self):
@@ -143,14 +154,15 @@ while not r == 4:
             while True:
                 akcja1 = input("strzel\\użyj przedmiotów: ")
                 if akcja1 == "strzel":
-                    akcja2 =  input(" w siebie\n w DEALERA")
+                    akcja2 =  input(" w siebie\n w DEALERA: ")
                     if akcja2 == "w siebie":
                         gracz1.strzel(gracz1)
                     elif akcja2 == "w DEALERA":
                         gracz1.strzel(gracz2)
                 elif akcja1 == "użyj przedmiotów":
-                    for i in range(len(gracz1.przedmioty)):
-                        print(f"{gracz1.przedmioty[i]}")
-                    akcja3 = input("")
+                    while True:
+                        for i in range(len(gracz1.przedmioty)):
+                            print(f"{gracz1.przedmioty[i]}")
+                        akcja3 = input("")
         break
     break
